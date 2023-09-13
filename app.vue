22b8fcfb64c2f594021b70b7642e102fe008128f
Amir <amirhkarimi888@gmail.com>
2023-09-13 23:32:59 +0330

    seoMeta was added to app.vue

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
     <NuxtLayout />
   </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Clothing Store',
+  ogTitle: 'Clothing Store',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+
+</script>
