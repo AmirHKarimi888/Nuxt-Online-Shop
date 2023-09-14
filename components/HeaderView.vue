c24d1a53a232a7a913a010bb643a7e1d39faee6b
Amir <amirhkarimi888@gmail.com>
2023-09-15 01:57:36 +0330

    Some little changes

--- components/HeaderView.vue
+++ components/HeaderView.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="w-full p-4 border-b border-gray-400 shadow-lg flex lg:gap-[36%] md:gap-[33%] sm:gap-[27%] max-sm:gap-[10%] grid-cols-3 justify-center items-center dark:bg-[#000000] dark:text-white dark:border-gray-600">
+    <nav class="w-full p-4 border-b border-gray-400 shadow-lg flex lg:gap-[36%] md:gap-[33%] sm:gap-[27%] max-sm:gap-[18%] grid-cols-3 justify-center items-center dark:bg-[#000000] dark:text-white dark:border-gray-600">
         <button @click="emit('toggleTheme')">
             <i id="themeToggleBtnIcon" class="fa"></i>
         </button>
